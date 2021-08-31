66fa70fcb110e09a833adba13b337ba0a552fd3a
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2021-08-31 21:22:29 +0200

    add a user route to handle signup

---
+++ server/routes/publicRoutes.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const { body } = require('express-validator/check');
+
+const User = require('../models/User');
+const publicController = require('../controllers/publicController');
+
+const router = express.Router();
+
+router.post(
+  'user',
+  [
+    body('username').trim().not().isEmpty(),
+    body('password').trim().isLength({ min: 5 }),
+    body('role').not().isEmpty().custom((value) => {
+      if (value !== "Admin" || value !== "Guest") {
+        throw new Error('role value should be Admin or Guest');
+      }
+      return true;
+    }),
+    body('phoneNumber').trim().isLength({ min: 5 }),
+  ],
+  publicController
+);
