ce9a231a0560020f1bdf7198d86071ee79a097ef
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2021-08-31 22:26:20 +0200

    add a function to handle signup

---
+++ server/controllers/publicController.js
@@ -0,0 +1,30 @@
+const { validationResult } = require('express-validator/check');
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
+
+exports.signup = async (req, res, next) => {
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    const error = new Error('Validation failed.');
+    error.statusCode = 422;
+    error.data = errors.array();
+    throw error;
+  }
+  try {
+    const { username, password, role, mobileNumber } = req.body;
+    let hashedPass = await bcrypt.hash(password, 12);
+    let user = new User({
+      username,
+      password: hashedPass,
+      role,
+      mobileNumber,
+    });
+    let savedUser = await user.save();
+    res.status(201).json({ message: 'User created!', userId: result._id });
+  } catch (error) {
+    if (!err.statusCode) {
+      err.statusCode = 500;
+    }
+    next(err);
+  }
+};
