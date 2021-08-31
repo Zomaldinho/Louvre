23695f6b16e04e15c60fdf89dc323e13cab3b2f5
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2021-08-31 23:34:14 +0200

    fix typo and logic errors

--- server/routes/publicRoutes.js
+++ server/routes/publicRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { body } = require('express-validator/check');
+const { body } = require('express-validator');
 
 const User = require('../models/User');
 const publicController = require('../controllers/publicController');
@@ -7,17 +7,19 @@ const publicController = require('../controllers/publicController');
 const router = express.Router();
 
 router.post(
-  'user',
+  '/user',
   [
     body('username').trim().not().isEmpty(),
     body('password').trim().isLength({ min: 5 }),
     body('role').not().isEmpty().custom((value) => {
-      if (value !== "Admin" || value !== "Guest") {
+      if (value !== "Admin" && value !== "Guest") {
         throw new Error('role value should be Admin or Guest');
       }
       return true;
     }),
-    body('phoneNumber').trim().isLength({ min: 5 }),
+    body('mobileNumber').trim().isLength({ min: 5 }),
   ],
-  publicController
+  publicController.signup
 );
+
+module.exports = router
