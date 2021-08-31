bae6a89a75175a50058d065dd9c9c34cc0fe4416
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2021-08-31 21:08:16 +0200

    add a console log to indicate the app is running

--- server/app.js
+++ server/app.js
@@ -12,4 +12,5 @@ mongoose
     'mongodb+srv://root:MHlDw6ve2wSxk64w@cluster0.306uy.mongodb.net/Louvre?retryWrites=true&w=majority'
   )
   .then((result) => app.listen(3000))
+  .then(() => console.log('app is running on port 3000'))
   .catch(console.log);
