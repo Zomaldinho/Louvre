6d1c398528a5c1076f7a89aefc47ea48aca02882
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2021-08-31 20:54:11 +0200

    add app.js initial code

---
+++ server/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose
+  .connect(
+    'mongodb+srv://root:MHlDw6ve2wSxk64w@cluster0.306uy.mongodb.net/Louvre?retryWrites=true&w=majority'
+  )
+  .then((result) => app.listen(3000))
+  .catch(console.log);
