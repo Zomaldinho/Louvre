872ff7447813ccbe30e8252e34ffaffaab299582
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2021-09-01 21:53:03 +0200

    add helper function for image upload

---
+++ server/helpers/uploadImage.js
@@ -0,0 +1,23 @@
+const multer = require('multer');
+
+
+exports.storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, './uploads');
+  },
+  filename: (req, file, cb) => {
+    cb(null, new Date().toISOString().replace(/:/g, '-') + file.originalname);
+  },
+});
+
+exports.fileFilter = (req, file, cb) => {
+  if (
+    file.mimetype === 'image/png' ||
+    file.mimetype === 'image/jpg' ||
+    file.mimetype === 'image/jpeg'
+  ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
