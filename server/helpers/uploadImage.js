5a08220b9dadb58fa24707e8f9daf69b7128d86f
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2021-09-01 23:43:26 +0200

    change image directory

--- server/helpers/uploadImage.js
+++ server/helpers/uploadImage.js
@@ -3,7 +3,7 @@ const multer = require('multer');
 
 exports.storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, './uploads');
+    cb(null, './images');
   },
   filename: (req, file, cb) => {
     cb(null, new Date().toISOString().replace(/:/g, '-') + file.originalname);
