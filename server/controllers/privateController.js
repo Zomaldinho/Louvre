d7ffc350e9f63206121ee56afc049e86c9295383
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2021-09-01 23:42:23 +0200

    add create art function

--- server/controllers/privateController.js
+++ server/controllers/privateController.js
@@ -1,6 +1,7 @@
 const { validationResult } = require('express-validator');
 
 const Art = require('../models/Art');
+const validationHelper = require('../helpers/validations')
 
 exports.getArts = async (req, res, next) => {
   const currentPage = req.body.page || 1;
@@ -13,6 +14,38 @@ exports.getArts = async (req, res, next) => {
     res.status(200).json({
       message: 'Fetched arts successfully.',
       arts,
+    });
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    next(error);
+  }
+};
+
+exports.createArt = async (req, res, next) => {
+  try {
+    const errors = validationResult(req);
+    // handle validation error
+    if (!errors.isEmpty()) {
+      validationHelper.handleValidationErrors(errors);
+    }
+    if (req.userRole !== 'Admin') {
+      const error = new Error('Only Admin users have access to this endpoint');
+      error.statusCode = 401;
+      throw error;
+    }
+    let { artist, description } = req.body;
+    let { path } = req.file;
+    let art = new Art({
+      artist,
+      description,
+      picture: path
+    })
+    let savedArt = await art.save()
+    res.status(201).json({
+      message: 'Art created successfully!',
+      art: savedArt,
     });
   } catch (error) {
     if (!error.statusCode) {
