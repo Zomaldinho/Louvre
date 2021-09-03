422a34db1d437f6d9f6d825d7db84e71ed990259
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2021-09-03 13:35:58 +0200

    add login component jsx

--- frontend/src/components/Login/Login.js
+++ frontend/src/components/Login/Login.js
@@ -1,12 +1,42 @@
-import React from 'react'
-import './Login.css'
+import React from 'react';
+import './Login.css';
 
 const Login = () => {
+  let usernamePlaceHolder = 'input your username here';
+  let passwordPlaceHolder = 'input your password here';
   return (
-    <div>
-      Login Component
+    <div className="main-login d-flex justify-content-md-center align-items-center vh-100 ">
+      <div className="card" style={{ width: '40%' }}>
+        <div className="card-header text-center">Login</div>
+        <form className="p-4">
+          <div className="mb-3">
+            <label className="form-label">Username</label>
+            <input
+              placeholder={usernamePlaceHolder}
+              type="text"
+              className="form-control"
+            />
+          </div>
+          <div className="mb-3">
+            <label htmlFor="exampleInputPassword1" className="form-label">
+              Password
+            </label>
+            <input
+              placeholder={passwordPlaceHolder}
+              type="password"
+              className="form-control"
+            />
+          </div>
+          <div>
+            <button type="submit" className="mx-auto btn btn-primary">
+              Login
+            </button>
+          </div>
+          <p className="mt-2">or <a href="#">Sign up</a> if you are a new Guest</p>
+        </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
