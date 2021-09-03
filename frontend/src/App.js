8f612516dd37e8bc493886b282684f2814cb1e2d
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2021-09-03 14:06:48 +0200

    add initial signup page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './components/Login/Login';
+import Signup from './components/Signup/Signup';
 
 function App() {
   return (
     <div className="App container bg-light">
-      <Login />
+      <Signup />
     </div>
   );
 }
