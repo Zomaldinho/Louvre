8d6d0b3ad3bc5c274130be38eb56aabb66667b24
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2021-09-03 16:03:40 +0200

    add hard coded art grid

--- frontend/src/components/ArtGrid/ArtGrid.js
+++ frontend/src/components/ArtGrid/ArtGrid.js
@@ -1,11 +1,19 @@
-import React from "react";
+import React from 'react';
+import ArtCard from '../ArtCard/ArtCard';
 
 const ArtGrid = () => {
   return (
     <div>
-      ArtGrid Component
+      <h2 className="text-start">Gallary</h2>
+      <div className="d-flex justify-content-evenly row row-cols-1 row-cols-sm-2 row-cols-md-5">
+        {Array(12)
+          .fill(1)
+          .map((el, i) => (
+            <ArtCard className="col" />
+          ))}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default ArtGrid
+export default ArtGrid;
