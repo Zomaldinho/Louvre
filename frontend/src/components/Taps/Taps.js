18c462ebc9fdc6984bd9670f4222b8c6c2dc098f
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2021-09-04 19:26:20 +0200

    make taps component dynamic

--- frontend/src/components/Taps/Taps.js
+++ frontend/src/components/Taps/Taps.js
@@ -1,19 +1,31 @@
 import React from 'react';
-import logoUser from "../../assets/nav/users/user.png"
-import logoArt from "../../assets/nav/art/supervised_user_circle.png"
+import { Link } from 'react-router-dom';
 
-const Taps = () => {
+import logoUser from '../../assets/nav/users/user.png';
+import logoArt from '../../assets/nav/art/supervised_user_circle.png';
+
+const Taps = (props) => {
+  if (props.page == 'users') {
+  } else if (props.page == 'arts') {
+  }
   return (
-    <ul class="nav nav-tabs">
-      <li class="nav-item">
-        <a class="nav-link active" aria-current="page" href="#">
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <Link
+          className={props.arts ? 'nav-link active' : 'nav-link'}
+          aria-current="page"
+          to="/admin/arts"
+        >
           Art Pieces <img src={logoArt}></img>
-        </a>
+        </Link>
       </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">
+      <li className="nav-item">
+        <Link
+          className={props.users ? 'nav-link active' : 'nav-link'}
+          to="/admin/users"
+        >
           Users <img src={logoUser}></img>
-        </a>
+        </Link>
       </li>
     </ul>
   );
