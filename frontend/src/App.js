80cad82d718de3aa26277254d91badb04a105833
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2021-09-04 15:52:55 +0200

    add initial side nav component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,13 +7,14 @@ import ArtGrid from './components/ArtGrid/ArtGrid';
 import UsersTable from './components/UsersTable/UsersTable';
 import ArtsTable from './components/ArtsTable/ArtsTable';
 import Navbar from './components/Navbar/Navbar';
+import SideNav from './components/SideNav/SideNav';
 
 function App() {
   return (
     <div>
       <Navbar />
+      <SideNav />
     <div className="App container bg-light">
-      <ArtsTable />
     </div>
 
     </div>
