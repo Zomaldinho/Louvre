f9f8d1e3529a4e13db333f7bf687a8a732cfa227
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2021-09-04 22:38:37 +0200

    return role in token when user login

--- server/controllers/publicController.js
+++ server/controllers/publicController.js
@@ -76,7 +76,13 @@ exports.login = async (req, res, next) => {
       'ExtremeSolution',
       { expiresIn: '1d' }
     );
-    res.status(200).json({ token: token, userId: loadedUser._id.toString() });
+    res
+      .status(200)
+      .json({
+        token: token,
+        userId: loadedUser._id.toString(),
+        role: loadedUser.role,
+      });
   } catch (error) {
     if (!error.statusCode) {
       error.statusCode = 500;
