4cc6af12b0d95d928fb19fd47bcc7ce483f94c05
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2021-09-04 17:18:14 +0200

    add admin arts page

---
+++ frontend/src/pages/AdminArtsPage.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ArtsTable from '../components/ArtsTable/ArtsTable';
+import Navbar from '../components/Navbar/Navbar';
+import Taps from '../components/Taps/Taps';
+
+const AdminArtsPage = () => {
+  return (
+    <div>
+      <Navbar />
+      <div className="App container bg-light">
+        <Taps />
+        <ArtsTable />
+      </div>
+    </div>
+  );
+};
+
+export default AdminArtsPage;
