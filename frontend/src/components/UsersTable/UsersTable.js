3aab77d7c21e8bda50248da83f82a6833be818ad
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2021-09-04 14:32:16 +0200

    add dummy data to table component

--- frontend/src/components/UsersTable/UsersTable.js
+++ frontend/src/components/UsersTable/UsersTable.js
@@ -1,11 +1,34 @@
-import React from "react";
+import React from 'react';
 
 const UsersTable = () => {
-  return(
-    <div>
-      Users Table
+  let users = [
+    { id: 'qwee', username: 'asdads', phoneNumber: '012213' },
+    { id: 'qwee2', username: 'asdads2', phoneNumber: '012213' },
+    { id: 'qwee3', username: 'asdads3', phoneNumber: '012213' },
+    { id: 'qwee4', username: 'asdads4', phoneNumber: '012213' },
+  ];
+  return (
+    <div className="p-2 bg-white">
+      <table className="table">
+        <thead className="table-light">
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">User Name</th>
+            <th scope="col">Phone Number</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.map((user, i) => (
+            <tr>
+              <th scope="row">{user.id}</th>
+              <td>{user.username}</td>
+              <td>{user.phoneNumber}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
-}
+  );
+};
 
-export default UsersTable
+export default UsersTable;
