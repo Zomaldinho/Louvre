d19ece99d670d2a83719caeb28a250419f7208c4
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2021-09-04 23:28:10 +0200

    handle token and home route

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,45 @@
 import './App.css';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 
 import Login from './components/Login/Login';
 import Signup from './components/Signup/Signup';
 import GuestGallaryArtsPage from './pages/GuestGallaryArtsPage';
 import AdminArtsPage from './pages/AdminArtsPage';
 import AdminUsersPage from './pages/AdminUsersPage';
+import { useEffect, useState } from 'react';
+import { verify } from 'jsonwebtoken'
 
 function App() {
+  const [token, setToken] = useState(null)
+  const [role, setRole] = useState('')
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    let token = localStorage.getItem('token')
+    if(token) {
+      setToken(token)
+      let decodedToken = verify(token, 'ExtremeSolution')
+      setRole(decodedToken.role)
+      console.log({role});
+    }
+    setLoading(false)
+  },[])
+
+  if(loading) return <div>Loading...</div>
+
   return (
     <div>
       <Switch>
+        <Route path="/" exact>
+          {token ? (
+            role == 'Admin' ? (
+              <Redirect to="/admin/arts" />
+            ) : (
+              <Redirect to="/gallery" />
+            )
+          ) : (
+            <Redirect to="/login" />
+          )}
+        </Route>
         <Route path="/login">
           <Login />
         </Route>
