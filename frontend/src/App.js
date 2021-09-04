0fccb7e8aeff9f3b2d8e5620949c56417b3ad625
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2021-09-04 23:41:16 +0200

    format code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,24 +7,25 @@ import GuestGallaryArtsPage from './pages/GuestGallaryArtsPage';
 import AdminArtsPage from './pages/AdminArtsPage';
 import AdminUsersPage from './pages/AdminUsersPage';
 import { useEffect, useState } from 'react';
-import { verify } from 'jsonwebtoken'
+import { verify } from 'jsonwebtoken';
 
 function App() {
-  const [token, setToken] = useState(null)
-  const [role, setRole] = useState('')
-  const [loading, setLoading] = useState(true)
+  const [token, setToken] = useState(null);
+  const [role, setRole] = useState('');
+  const [loading, setLoading] = useState(true);
+  
   useEffect(() => {
-    let token = localStorage.getItem('token')
-    if(token) {
-      setToken(token)
-      let decodedToken = verify(token, 'ExtremeSolution')
-      setRole(decodedToken.role)
-      console.log({role});
+    let token = localStorage.getItem('token');
+    if (token) {
+      setToken(token);
+      let decodedToken = verify(token, 'ExtremeSolution');
+      setRole(decodedToken.role);
+      console.log({ role });
     }
-    setLoading(false)
-  },[])
+    setLoading(false);
+  }, []);
 
-  if(loading) return <div>Loading...</div>
+  if (loading) return <div>Loading...</div>;
 
   return (
     <div>
@@ -50,15 +51,21 @@ function App() {
             <Signup />
           </Route>
         )}
-        {token && role == 'Guest' && <Route path="/gallery">
-          <GuestGallaryArtsPage />
-        </Route>}
-        {token && role == 'Admin' && <Route path="/admin/arts">
-          <AdminArtsPage />
-        </Route>}
-        {token && role == 'Admin' && <Route path="/admin/users">
-          <AdminUsersPage />
-        </Route>}
+        {token && role == 'Guest' && (
+          <Route path="/gallery">
+            <GuestGallaryArtsPage />
+          </Route>
+        )}
+        {token && role == 'Admin' && (
+          <Route path="/admin/arts">
+            <AdminArtsPage />
+          </Route>
+        )}
+        {token && role == 'Admin' && (
+          <Route path="/admin/users">
+            <AdminUsersPage />
+          </Route>
+        )}
         <Route path="*">
           <Redirect to="/" />
         </Route>
