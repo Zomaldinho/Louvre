13975cd59b4defc3fa9d58277bbc49be126af3a6
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2021-09-04 14:15:03 +0200

    add initial Users Table component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,11 +4,12 @@ import Login from './components/Login/Login';
 import Signup from './components/Signup/Signup';
 import ArtCard from './components/ArtCard/ArtCard';
 import ArtGrid from './components/ArtGrid/ArtGrid';
+import UsersTable from './components/UsersTable/UsersTable';
 
 function App() {
   return (
     <div className="App container bg-light">
-      <ArtGrid />
+      <UsersTable />
     </div>
   );
 }
---
+++ frontend/src/components/UsersTable/UsersTable.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const UsersTable = () => {
+  return(
+    <div>
+      Users Table
+    </div>
+  )
+}
+
+export default UsersTable
