df1bdb41d9f43d02bbd89cfff6e3013c3b08c0ea
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2021-09-05 09:23:56 +0200

    add signup page logic and functions

--- frontend/src/components/Signup/Signup.js
+++ frontend/src/components/Signup/Signup.js
@@ -1,11 +1,53 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import { errorHandler, signUpSuccessfulMessage } from '../../helpers/Popups';
 import "./Signup.css"
 
 const Signup = () => {
   let usernamePlaceHolder = 'input your username here';
   let passwordPlaceHolder = 'input your password here';
   let phoneNumberPlaceHolder = 'input your password here';
+  let history = useHistory()
+
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [role, setRole] = useState('')
+  const [mobileNumber, setMobileNumber] = useState('')
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value)
+  }
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value)
+  }
+
+  const handleRoleChange = (event) => {
+    setRole(event.target.value)
+  }
+
+  const handleMobileNumberChange = (event) => {
+    setMobileNumber(event.target.value)
+  }
+
+  const handleSignupSubmitForm = async (event) => {
+    event.preventDefault();
+    let res = await fetch('http://localhost:5000/public/user', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ username, password, role, mobileNumber }),
+    });
+
+    if (res.status == 422)
+      return errorHandler('Validation failed. Make sure the username isn\'t used yet!');
+    if (res.status != 201)
+      return errorHandler('Something went wrong, please try again');
+
+    res = await res.json()
+    signUpSuccessfulMessage()
+    history.replace('/login')
+  }
+
   return(
     <div className="main-login d-flex justify-content-md-center align-items-center vh-100 ">
       <div className="card login-card mx-auto">
@@ -15,6 +57,7 @@ const Signup = () => {
             <label className="form-label">Username</label>
             <input
               placeholder={usernamePlaceHolder}
+              onChange={handleUsernameChange}
               type="text"
               className="form-control"
             />
@@ -25,14 +68,15 @@ const Signup = () => {
             </label>
             <input
               placeholder={phoneNumberPlaceHolder}
-              type="test"
+              onChange={handleMobileNumberChange}
+              type="text"
               className="form-control"
             />
           </div>
           <div className="form-group">
             <label className="form-label" >User Type</label>
-            <select className="form-control" aria-label="Default select example">
-              <option value="Guest">Guest</option>
+            <select onChange={handleRoleChange} className="form-control" aria-label="Default select example">
+              <option selected value="Guest">Guest</option>
               <option value="Admin">Admin</option>
             </select>
           </div>
@@ -41,6 +85,7 @@ const Signup = () => {
               Password
             </label>
             <input
+              onChange={handlePasswordChange}
               placeholder={passwordPlaceHolder}
               type="password"
               className="form-control"
@@ -48,7 +93,7 @@ const Signup = () => {
           </div>
 
           <div>
-            <button type="submit" className="mx-auto btn btn-primary">
+            <button onClick={handleSignupSubmitForm} type="submit" className="mx-auto btn btn-primary">
               Sign Up
             </button>
           </div>
