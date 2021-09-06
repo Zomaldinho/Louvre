e5a305489a9aec1cdc2e76b7e58b09077601f5fe
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2021-09-06 16:06:28 +0200

    add handle logout function to the navbar component

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -1,13 +1,17 @@
-import React from "react";
+import React, { useContext } from "react";
+import { AuthContext } from "../../store/auth-context";
 
 const Navbar = () => {
+  let handleLogout = useContext(AuthContext).handleLogout
+
   return (
     <div className="border-bottom d-flex justify-content-between bg-white">
       <h1 className="ms-5">Louvre</h1>
-      <div className="me-5">
+      <div className="">
         <h5 className="m-0">{localStorage.getItem('username')}</h5>
-        <p>{localStorage.getItem('role')}</p>
+        <p className="m-0">{localStorage.getItem('role')}</p>
       </div>
+        <button onClick={handleLogout} className="me-5 btn btn-outline-danger">logout</button>
     </div>
   )
 }
