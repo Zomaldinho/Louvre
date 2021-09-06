8e2ab8b5c7eeb6c5c9753b4c80f93c34c7c9fa49
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2021-09-06 18:28:34 +0200

    replace long validations with functions

--- server/routes/publicRoutes.js
+++ server/routes/publicRoutes.js
@@ -1,36 +1,12 @@
 const express = require('express');
 const { body } = require('express-validator');
 
-const User = require('../models/User');
 const publicController = require('../controllers/publicController');
+const validationHelper = require('../helpers/validations')
 
 const router = express.Router();
 
-router.post(
-  '/user',
-  [
-    body('username').trim().not().isEmpty().toLowerCase(),
-    body('password').trim().isLength({ min: 5 }),
-    body('role')
-      .not()
-      .isEmpty()
-      .custom((value) => {
-        if (value !== 'Admin' && value !== 'Guest') {
-          throw new Error('role value should be Admin or Guest');
-        }
-        return true;
-      }),
-    body('mobileNumber').trim().isLength({ min: 5 }),
-  ],
-  publicController.signup
-);
-router.post(
-  '/login',
-  [
-    body('username').trim().not().isEmpty().toLowerCase(),
-    body('password').trim().not().isEmpty(),
-  ],
-  publicController.login
-);
+router.post('/user', validationHelper.createUser(), publicController.signup);
+router.post('/login', validationHelper.login(), publicController.login);
 
 module.exports = router;
