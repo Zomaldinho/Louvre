b81085c0fa1f3d55945a06c9432bf2b0ca18aab2
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2021-09-06 18:28:01 +0200

    add validation functions for create user and login

--- server/helpers/validations.js
+++ server/helpers/validations.js
@@ -7,6 +7,30 @@ exports.createArt = () => {
   ]
 }
 
+exports.createUser = () => {
+  return [
+    body('username').trim().not().isEmpty().toLowerCase(),
+    body('password').trim().isLength({ min: 5 }),
+    body('role')
+      .not()
+      .isEmpty()
+      .custom((value) => {
+        if (value !== 'Admin' && value !== 'Guest') {
+          throw new Error('role value should be Admin or Guest');
+        }
+        return true;
+      }),
+    body('mobileNumber').trim().isLength({ min: 5 }),
+  ]
+}
+
+exports.login = () => {
+  return [
+    body('username').trim().not().isEmpty().toLowerCase(),
+    body('password').trim().not().isEmpty(),
+  ]
+}
+
 exports.handleValidationErrors = (errors) => {
   const error = new Error('Validation failed.');
   error.statusCode = 422;
