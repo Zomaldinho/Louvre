7854e7b23ebcc3b033740f0c43c8be139d7f38f0
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2021-09-06 13:24:04 +0200

    format code

--- frontend/src/components/ArtsTable/ArtsTable.js
+++ frontend/src/components/ArtsTable/ArtsTable.js
@@ -1,65 +1,74 @@
-import React, { useEffect, useState } from "react";
-import img from '../../assets/IconBackground-1.png'
-import Paginator from "../Paginator/Paginator";
-import './ArtsTable.css'
+import React, { useEffect, useState } from 'react';
+import Paginator from '../Paginator/Paginator';
+import './ArtsTable.css';
 
 const ArtsTable = () => {
-  const [arts, setArts] = useState([])
-  const [count, setCount] = useState([])
-  let baseURL = 'http://localhost:5000/'
+  const [arts, setArts] = useState([]);
+  const [count, setCount] = useState([]);
+  let baseURL = 'http://localhost:5000/';
 
   useEffect(async () => {
-    await updateArts(1)
+    await updateArts(1);
   }, []);
 
   const updateArts = async (page) => {
-    let res = await fetch(`http://localhost:5000/private/getArts?page=${page}`, {
-      method: 'GET',
-      headers: {
-        Authorization: 'JWT ' + localStorage.getItem('token'),
-      },
-    });
-    res = await res.json()
-    setArts(res.arts)
-    setCount(res.count)
-  }
+    let res = await fetch(
+      `http://localhost:5000/private/getArts?page=${page}`,
+      {
+        method: 'GET',
+        headers: {
+          Authorization: 'JWT ' + localStorage.getItem('token'),
+        },
+      }
+    );
+    res = await res.json();
+    setArts(res.arts);
+    setCount(res.count);
+  };
 
   const handlePaginatorChange = async (newPage) => {
-    await updateArts(newPage)
-  }
+    await updateArts(newPage);
+  };
 
   return (
     <div>
       <h2 className="mb-3 pt-3 text-start">Art pieces</h2>
       <div className="p-2 bg-white">
-      <table className="table align-middle">
-        <thead className="table-light">
-          <tr>
-            <th scope="col">Item</th>
-            <th scope="col">Name</th>
-            <th scope="col">Artist</th>
-            <th scope="col">Description</th>
-            <th scope="col">Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {arts.map((art, i) => (
+        <table className="table align-middle">
+          <thead className="table-light">
             <tr>
-              <td><img className="img-thumbnail item-img" src={baseURL + art.picture} /></td>
-              <td>Art Piece</td>
-              <td>{art.artist}</td>
-              <td>{art.description}</td>
-              <td><button className="btn btn-danger">Delete</button></td>
+              <th scope="col">Item</th>
+              <th scope="col">Name</th>
+              <th scope="col">Artist</th>
+              <th scope="col">Description</th>
+              <th scope="col">Action</th>
             </tr>
-          ))}
-        </tbody>
-      </table>
+          </thead>
+          <tbody>
+            {arts.map((art, i) => (
+              <tr>
+                <td>
+                  <img
+                    className="img-thumbnail item-img"
+                    src={baseURL + art.picture}
+                  />
+                </td>
+                <td>Art Piece</td>
+                <td>{art.artist}</td>
+                <td>{art.description}</td>
+                <td>
+                  <button className="btn btn-danger">Delete</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+      <div className="d-flex justify-content-center m-3">
+        <Paginator count={count} change={handlePaginatorChange} />
+      </div>
     </div>
-    <div className='d-flex justify-content-center m-3'>
-      <Paginator count={count} change={handlePaginatorChange} />
-    </div>
-    </div>
-  )
-}
+  );
+};
 
-export default ArtsTable
+export default ArtsTable;
