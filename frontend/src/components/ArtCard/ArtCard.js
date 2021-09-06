12f5d2ebecfb4d91839b0aedfe0040a6d2fb5713
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2021-09-06 11:08:44 +0200

    make artCard dynamic

--- frontend/src/components/ArtCard/ArtCard.js
+++ frontend/src/components/ArtCard/ArtCard.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import img from '../../assets/IconBackground-1.png'
 
-const ArtCard = () => {
+const ArtCard = (props) => {
+  let baseURL = 'http://localhost:5000/'
   return (
     <div class="card m-3 p-0" style={{ borderRadius: '25px'}}>
-      <img src={img} class="card-img-top" alt="..." />
+      <img src={baseURL + props.art.picture} class="card-img-top" alt="..." />
       <div class="card-body">
         <p class="text-start card-text" style={{color: 'blue'}}>
-          Some quick 
+          {props.art.artist}
         </p>
       </div>
     </div>
