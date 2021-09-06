7c9edbe563786c034bcb5b5d5e95a27cfdd506de
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2021-09-06 11:04:05 +0200

    add request to get arts in gallery page

--- frontend/src/components/ArtGrid/ArtGrid.js
+++ frontend/src/components/ArtGrid/ArtGrid.js
@@ -1,15 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ArtCard from '../ArtCard/ArtCard';
 
 const ArtGrid = () => {
+  const [arts, setArts] = useState([])
+
+  useEffect(async () => {
+    let res = await fetch('http://localhost:5000/private/getArts?page=1', {
+      method: 'GET',
+      headers: {
+        Authorization: 'JWT ' + localStorage.getItem('token'),
+      },
+    });
+    res = await res.json()
+    setArts(res.arts)
+  }, []);
+
   return (
     <div>
       <h2 className="text-start">Gallary</h2>
       <div className="d-flex justify-content-evenly row row-cols-1 row-cols-sm-2 row-cols-md-5">
-        {Array(12)
-          .fill(1)
-          .map((el, i) => (
-            <ArtCard className="col" />
+        {!!arts.length && arts
+          .map((art, i) => (
+            <ArtCard art={art} className="col" />
           ))}
       </div>
     </div>
