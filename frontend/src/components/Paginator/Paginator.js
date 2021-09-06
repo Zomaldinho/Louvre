dcc809c6ab65fd61f83d95cbd6dbd5506bafbca5
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2021-09-06 12:02:01 +0200

    add paginator functionality for nex, prev and buttons click

--- frontend/src/components/Paginator/Paginator.js
+++ frontend/src/components/Paginator/Paginator.js
@@ -5,15 +5,37 @@ import right from "../../assets/Right.svg"
 
 const Paginator = (props) => {
   const [pagesNumber, setPagesNumber] = useState(0)
+  const [currentPageNumber, setCurrentPageNumber] = useState(1)
 
   useEffect(() => {
     setPagesNumber(Math.ceil(props.count/12))
   }, [props.count]);
 
+  const handlePageNumberChange = (newPage) => {
+    if(newPage != currentPageNumber){
+      setCurrentPageNumber(newPage)
+      props.change(newPage)
+    }
+  }
+
+  const handleNextClick = () => {
+    if(currentPageNumber < pagesNumber){
+      setCurrentPageNumber(currentPageNumber + 1)
+      props.change(currentPageNumber + 1)
+    }
+  }
+
+  const handlePreviousClick = () => {
+    if(currentPageNumber > 1){
+      setCurrentPageNumber(currentPageNumber - 1)
+      props.change(currentPageNumber - 1)
+    }
+  }
+
   return (
     <nav aria-label="Page navigation example">
       <ul class="pagination">
-        <li class="page-item">
+        <li onClick={handlePreviousClick} class="page-item">
           <a class="page-link">
             <img src={left} /> Previous
           </a>
@@ -21,11 +43,11 @@ const Paginator = (props) => {
         {Array(pagesNumber)
           .fill(1)
           .map((e, i) => (
-            <li class="page-item">
+            <li onClick={()=>handlePageNumberChange(i+1)} class="page-item">
               <a class="page-link">{i + 1}</a>
             </li>
           ))}
-        <li class="page-item">
+        <li onClick={handleNextClick} class="page-item">
           <a class="page-link">
             Next <img src={right} />
           </a>
