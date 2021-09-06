9f4e240b978f936febdd290fb6ad8ba1383c77ed
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2021-09-06 11:08:19 +0200

    make navbar dynamic

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -5,8 +5,8 @@ const Navbar = () => {
     <div className="border-bottom d-flex justify-content-between bg-white">
       <h1 className="ms-5">Louvre</h1>
       <div className="me-5">
-        <h5 className="m-0">Name</h5>
-        <p>Admin</p>
+        <h5 className="m-0">{localStorage.getItem('username')}</h5>
+        <p>{localStorage.getItem('role')}</p>
       </div>
     </div>
   )
