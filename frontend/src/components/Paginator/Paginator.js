28e7a31642fa08363369c384555cdecf2b1ed618
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2021-09-06 11:39:40 +0200

    display pages count based on incoming items

--- frontend/src/components/Paginator/Paginator.js
+++ frontend/src/components/Paginator/Paginator.js
@@ -1,35 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import left from "../../assets/Left.svg"
 import right from "../../assets/Right.svg"
 
 
-const Paginator = () => {
+const Paginator = (props) => {
+  const [pagesNumber, setPagesNumber] = useState(0)
+
+  useEffect(() => {
+    setPagesNumber(Math.ceil(props.count/12))
+  }, [props.count]);
+
   return (
     <nav aria-label="Page navigation example">
       <ul class="pagination">
         <li class="page-item">
-          <a class="page-link" >
-             <img src={left}/> Previous
+          <a class="page-link">
+            <img src={left} /> Previous
           </a>
         </li>
+        {Array(pagesNumber)
+          .fill(1)
+          .map((e, i) => (
+            <li class="page-item">
+              <a class="page-link">{i + 1}</a>
+            </li>
+          ))}
         <li class="page-item">
-          <a class="page-link" >
-            1
-          </a>
-        </li>
-        <li class="page-item">
-          <a class="page-link" >
-            2
-          </a>
-        </li>
-        <li class="page-item">
-          <a class="page-link" >
-            3
-          </a>
-        </li>
-        <li class="page-item">
-          <a class="page-link" >
-            Next <img src={right}/>
+          <a class="page-link">
+            Next <img src={right} />
           </a>
         </li>
       </ul>
