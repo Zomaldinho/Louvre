897cad04040b43d021e5488f66404f2ea9887ec7
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2021-09-06 16:40:12 +0200

    change style

--- frontend/src/components/ArtGrid/ArtGrid.js
+++ frontend/src/components/ArtGrid/ArtGrid.js
@@ -29,7 +29,7 @@ const ArtGrid = () => {
   return (
     <div>
       <h2 className="text-start">Gallary</h2>
-      <div className="d-flex justify-content-evenly row row-cols-1 row-cols-sm-2 row-cols-md-5">
+      <div className="d-flex justify-content-evenly row row-cols-1 row-cols-sm-2 row-cols-md-4">
         {!!arts.length && arts
           .map((art, i) => (
             <ArtCard art={art} className="col" />
