37708ac11409f6687a7bce92de25f076540371f5
Hazem Elsayed <hazem_ahmed_93@yahoo.com>
2021-09-06 16:12:29 +0200

    add state to handle on card click

--- frontend/src/components/ArtCard/ArtCard.js
+++ frontend/src/components/ArtCard/ArtCard.js
@@ -1,15 +1,25 @@
-import React from 'react';
-import img from '../../assets/IconBackground-1.png'
+import React, { useState } from 'react';
 
 const ArtCard = (props) => {
-  let baseURL = 'http://localhost:5000/'
+  const [isClicked, setIsClicked] = useState(false);
+  let baseURL = 'http://localhost:5000/';
   return (
-    <div class="card m-3 p-0" style={{ borderRadius: '25px'}}>
-      <img src={baseURL + props.art.picture} class="card-img-top" alt="..." />
-      <div class="card-body">
-        <p class="text-start card-text" style={{color: 'blue'}}>
-          {props.art.artist}
-        </p>
+    <div>
+      <div
+        onClick={() => setIsClicked(true)}
+        class="card m-3 p-0"
+        style={{ borderRadius: '25px' }}
+      >
+        <img
+          src={baseURL + props.art.picture}
+          class="card-img-top"
+          alt="..."
+        />
+        <div class="card-body">
+          <p class="text-start card-text" style={{ color: 'blue' }}>
+            {props.art.artist}
+          </p>
+        </div>
       </div>
     </div>
   );
